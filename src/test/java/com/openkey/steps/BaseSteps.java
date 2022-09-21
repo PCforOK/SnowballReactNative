5062ddf3084a3eb429145983d0968ee9b54d30bf
ankiteno8 <ankit.sr@eno8.com>
2022-09-21 14:55:09 +0530

    Feature File Rename

--- src/test/java/com/openkey/steps/BaseSteps.java
+++ src/test/java/com/openkey/steps/BaseSteps.java
@@ -7,12 +7,15 @@ import io.appium.java_client.android.AndroidDriver;
 public class BaseSteps extends CapabilitiesManager {
     protected RN_OpenKeyMobileAndroidAppLoginScreen openKeyMobileAndroidAppLoginScreen;
     protected RN_OpenKeyMobileAndroidAppLaunchScreen openKeyMobileAndroidAppLaunchScreen;
+    protected OpenKeyDownloadMobileKeyScreen openKeyDownloadMobileKeyScreen;
+    protected OpenKeyDoorLockScreen openDoorLockScreen;
 
     public void setupScreens(AndroidDriver driver) {
 
         openKeyMobileAndroidAppLaunchScreen = new RN_OpenKeyMobileAndroidAppLaunchScreen(driver);
         openKeyMobileAndroidAppLoginScreen = new RN_OpenKeyMobileAndroidAppLoginScreen(driver);
-
+        openDoorLockScreen = new OpenKeyDoorLockScreen(driver);
+        openKeyDownloadMobileKeyScreen = new OpenKeyDownloadMobileKeyScreen(driver);
 
     }
 

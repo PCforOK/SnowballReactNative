5bc5da2124e51d135627112aaebcde387f0f6bc9
ankiteno8 <ankit.sr@eno8.com>
2022-12-06 11:43:19 +0530

    OK-3798-Execution flow abort on Failed test

--- src/test/java/com/openkey/basetest/TestRunner.java
+++ src/test/java/com/openkey/basetest/TestRunner.java
@@ -1,85 +1,116 @@
+
+
+
+
 package com.openkey.basetest;
 
 import com.openkey.setups.CapabilitiesManager;
 import com.openkey.utils.AllureReporting;
 import com.openkey.utils.DataBaseHandler;
-import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.FeatureWrapper;
-import io.cucumber.testng.PickleWrapper;
-import io.cucumber.testng.TestNGCucumberRunner;
+import com.openkey.utils.HooksManager;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
+import io.cucumber.testng.*;
+
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
-@CucumberOptions(
-        plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm","com.openkey.utils.HooksManager"},
-        monochrome = true,
-        tags = "@AppLaunchRNAndroidApp or @AppLoginRNAndroidApp",// or @OpenKeyDoorLockReactNativeApp", //or @DownloadMobileKey or ,
-        features = "src/main/com.openkey.resources/features",
-        glue = {"com.openkey.steps","com/openkey/utils"},
-        publish = true,
-        dryRun = false
-)
+    @CucumberOptions(
+            plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm","com.openkey.utils.HooksManager"},
+            monochrome = true,
+            tags = "@AppLaunchRNAndroidApp or @AppLoginRNAndroidApp or @OpenKeyDoorLockReactNativeApp",
+            features = "src/main/com.openkey.resources/features",
+            glue = {"com.openkey.steps","com/openkey/utils"},
+            publish = true,
+            dryRun = false
+    )
 
-public class TestRunner extends CapabilitiesManager {
+    public class TestRunner extends CapabilitiesManager{
 
-    private TestNGCucumberRunner testNGCucumberRunner;
-    static AllureReporting allureReporting ;
-static DataBaseHandler dataBaseHandler;
-    public TestRunner() throws IOException {
+        public Scenario scening;
+        ITestResult result;
 
-        allureReporting = new AllureReporting();
-        dataBaseHandler=new DataBaseHandler();
+        private TestNGCucumberRunner testNGCucumberRunner;
+        static AllureReporting allureReporting ;
+        static DataBaseHandler dataBaseHandler;
+
+        public TestRunner() throws IOException {
+
+          allureReporting = new AllureReporting();
+          dataBaseHandler=new DataBaseHandler();
+
+        }
+
+        @BeforeClass(alwaysRun = true)
+        public void setUpClass() {
+
+          testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+
+        }
+
+        @BeforeTest(alwaysRun = true)
+
+        public void cleanOldReportsData() throws IOException {
+
+          allureReporting.cleanUpAllureDirectory();
+
+        }
+
+
+        @Test(groups = "cucumber", description = "Run Cucumber Features.", dataProvider = "scenarios")
+        public void scenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
+
+
+            testNGCucumberRunner.runScenario(pickleWrapper.getPickle());
+
+
+            }
+
+
+
+     @AfterMethod
+       public void haltExecutionOnFailure(ITestResult result) {
+
+         if (result.getStatus() == ITestResult.FAILURE) {
+
+             testNGCucumberRunner.finish();
+
+         }
+      }
+
+
+
+       @DataProvider
+        public Object[][] scenarios() {
+
+          return testNGCucumberRunner.provideScenarios();
+
+        }
+
+       @AfterTest (alwaysRun = true)
+
+        public void createpropFile() throws IOException, SQLException, ClassNotFoundException {
+
+          allureReporting.envFileWriter();
+           allureReporting.executorFileWriter();
+            dataBaseHandler.setupConnection();
+
+        }
+
+
+       @AfterClass(alwaysRun = true)
+        public void tearDownClass()  {
+
+
+           testNGCucumberRunner.finish();
+
+        }
+
     }
 
-    @BeforeClass(alwaysRun = true)
-    public void setUpClass() {
-
-        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-
-    }
-
-    @BeforeTest(alwaysRun = true)
-
-    public void cleanOldReportsData() throws IOException {
-
-        allureReporting.cleanUpAllureDirectory();
-
-    }
 
 
-    @Test(groups = "cucumber", description = "Run Cucumber Features.", dataProvider = "scenarios")
-    public void scenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
 
-        testNGCucumberRunner.runScenario(pickleWrapper.getPickle());
-
-    }
-
-    @DataProvider
-    public Object[][] scenarios() {
-
-        return testNGCucumberRunner.provideScenarios();
-
-    }
-
-@AfterTest (alwaysRun = true)
-
-public void createpropFile() throws IOException, SQLException, ClassNotFoundException {
-
-        allureReporting.envFileWriter();
-    allureReporting.executorFileWriter();
-    dataBaseHandler.setupConnection();
-
-    }
-
-
-    @AfterClass(alwaysRun = true)
-    public void tearDownClass() throws IOException {
-
-
-        testNGCucumberRunner.finish();
-
-    }
-
-}
